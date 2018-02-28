06269b25e3c6d2987e59d4621a4627d3b3b6c553
sdrzymala <slawomirdrzymala@outlook.com>
2018-03-01 00:05:41 +0100

    Add simple method to get the files

--- ConsoleApp_download_helper/download_helper/DownloadHelper.cs
+++ ConsoleApp_download_helper/download_helper/DownloadHelper.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
 
 namespace download_helper
 {
@@ -9,7 +12,27 @@ namespace download_helper
 
         public void GetAllFiles()
         {
+            List<string> allFilesToDownload = GetAllFilesToDownloadFromConfigFile();
 
+            foreach(var file in allFilesToDownload)
+            {
+                DownloadSingleFile(file);
+            }
+        }
+
+        private List<string> GetAllFilesToDownloadFromConfigFile()
+        {
+            return new List<string>(File.ReadAllLines(ConfigFilePath));; 
+        }
+
+        private void DownloadSingleFile(string FileUrl)
+        {
+            WebClient Client = new WebClient();
+
+            string currentFileName = Path.GetFileName(FileUrl);
+            string currentFileOutputPath = Path.Combine(DownloadDirectory, currentFileName);
+
+            Client.DownloadFile(FileUrl, currentFileOutputPath);
         }
     }
 }
