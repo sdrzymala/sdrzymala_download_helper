375c2193c5169add7510585920bfe2fc938da4dd
sdrzymala <slawomirdrzymala@outlook.com>
2018-02-28 23:37:57 +0100

    Add new project - console app

---
+++ ConsoleApp_download_helper/ConsoleApp_download_helper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp_download_helper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
